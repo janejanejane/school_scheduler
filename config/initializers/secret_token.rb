619dea77e2782081088628cccc2f11f840a3f769
janejanejane <jeanclaudetteambait@gmail.com>
2012-12-19 20:39:44 +0800

    First commit basic setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchoolScheduler::Application.config.secret_token = '2c241cbf0f2b020813959a1d6e2c45d5cd91a715dacc135a9d17bd91f567e702045c57ac939da5eba55affb407edae7676af4c8ec87dc174ab127f449848efc1'
